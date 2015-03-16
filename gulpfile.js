a49342eda7cbe204afe9fa58b6ff857476375e6a
Ron Yu <ronyu77@gmail.com>
2015-03-16 14:03:46 +0800

    first version for nweportfolio

---
+++ gulpfile.js
@@ -0,0 +1,103 @@
+var gulp = require('gulp'),
+    uglyfly = require('gulp-uglyfly'),
+    stylus = require('gulp-stylus'),
+    plumber = require('gulp-plumber'),
+    jade = require('gulp-jade'),
+    livereload = require('gulp-livereload'),
+    imagemin = require('gulp-imagemin'),
+    prefix = require('gulp-autoprefixer'),
+    notify = require("gulp-notify"),
+    clean = require('gulp-clean'),
+    gulpFilter = require('gulp-filter'),
+    mainBowerFiles = require('main-bower-files');
+
+var js_dest_path = 'build/assets/lib/js';
+var css_dest_path = 'build/assets/lib/css';
+var img_for_fancybox = 'assets/lib/css';
+var themes_for_semantic_ui = 'bower_components/semantic-ui/dist/themes/**/*';
+
+var jsFilter = gulpFilter('*.js');
+var cssFilter = gulpFilter('*.css');
+var imgFilter = gulpFilter(['*.gif', '*.png']);
+
+
+//test task
+gulp.task('test', function(){
+  console.log("this is a test sentence.");
+});
+
+// export bower files to where we want
+gulp.task('exportBowerFiles', function() {
+    return gulp.src(mainBowerFiles())
+
+    .pipe(jsFilter)
+        .pipe(gulp.dest(js_dest_path))
+        .pipe(jsFilter.restore())
+        
+    .pipe(cssFilter)
+        .pipe(gulp.dest(css_dest_path))
+        .pipe(cssFilter.restore());
+});
+
+
+// export semantic-ui themes to where we want
+gulp.task('semanticUiThemes', function() {
+  return gulp.src(themes_for_semantic_ui, { base: 'bower_components/semantic-ui/dist' })
+        .pipe(gulp.dest('build/assets/lib/css'));
+
+});
+
+
+// Sctipt Task
+// Uglifies
+gulp.task('scripts', function(){
+  gulp.src('native/js/*.js')
+    .pipe(plumber())
+    .pipe(uglyfly())
+    .pipe(gulp.dest('build/js'))
+    .pipe(notify("Scripts uglify are done!"));
+});
+
+//Styles Task
+//compile 
+gulp.task('styles', function () {
+  gulp.src('native/stylus/**/*.styl')
+    .pipe(plumber())
+    .pipe(stylus())
+    .pipe(prefix())
+    .pipe(gulp.dest('build/css'))
+    .pipe(notify("Styles complie complete!"));
+});
+
+//Template task
+//compile jade
+gulp.task('templates', function () {
+  gulp.src('native/jade/index.jade')
+    .pipe(plumber())
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest('build'))
+    .pipe(notify("Template complie complete!"));
+});
+
+//Images task
+//compress
+gulp.task('image', function () {
+  gulp.src('native/img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('build/img'))
+    .pipe(notify("Images compress complete!"));
+});
+
+//Watch Task
+//Watches JS
+gulp.task('watch', function(){
+  var server = livereload();
+  gulp.watch('native/js/*.js', ['scripts']);
+  gulp.watch('native/stylus/**/*.styl', ['styles']);
+  gulp.watch('native/**/*.jade', ['templates']);
+});
+
+
+gulp.task('default', ['scripts', 'styles','templates', 'watch']);
---
+++ native/js/basic.js
@@ -0,0 +1,75 @@
+$(function() {
+
+  var $m_btn = $("#m_btn"),
+    $m_menu = $("#m_menu"),
+    $menu = $("#menu"),
+    $window = $(window);
+
+  if (navigator.userAgent.indexOf("Firefox") != -1) {
+    $('header').animate({
+      'border-spacing': 50
+    }, {
+      step: function(now, fx) {
+        $(fx.elem).css("background-position", "50% " + now + "%");
+      },
+      duration: 2000
+    });
+  } else {
+    $("header").animate({
+      backgroundPositionY: "100%"
+    }, 2000);
+  }
+
+
+  // show menu smoothly
+  setTimeout(function() {
+    $menu.animate({
+      "opacity": "1",
+      "top": "80px"
+    }, 1000, "easeOutQuint");
+  }, 1000);
+
+
+
+  // show sidebar whem user clicks mobile menu 
+  $m_btn.on('click', function() {
+    $m_menu
+      .sidebar('setting', 'transition', 'push')
+      .sidebar("toggle");
+  });
+
+
+  // hide sidebar menu when screen > 768px
+  $window.on('resize', function() {
+    var winW = $window.width();
+    if (winW > 768) {
+      $m_menu.sidebar("hide");
+    }
+  });
+
+  // make main menu sticky on the top when scrolling
+  $menu.each(function() {
+    var menuOffsetTop = $menu.offset().top;
+
+    $window.on('scroll', $.throttle(1000 / 15, function() {
+      if ($window.scrollTop() > menuOffsetTop + 80) {
+        $menu.addClass('inverted').removeClass("secondary").css({
+          "position": 'fixed',
+          "top": '0px'
+        });
+      } else {
+        $menu.removeClass('inverted').addClass("secondary").css({
+          "position": 'absolute',
+          "top": '80px'
+        });
+      }
+    }));
+  });
+
+  // make menu item active when been clicked
+  $(".menu .item").on('click', function(event) {
+    event.preventDefault();
+    $(".menu .item").removeClass('active');
+    $(this).addClass('active');
+  });
+});
