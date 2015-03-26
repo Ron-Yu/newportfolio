5be0c1e2a938391b16eff60b02a7f73a58e06412
Ron Yu <ronyu77@gmail.com>
2015-03-26 17:12:09 +0800

    skel.js grid system test

--- native/js/init.js
+++ native/js/init.js
@@ -6,22 +6,26 @@ skel.init({
         large: {
             media: '(min-width: 1025px)',
             href: 'css/responsive/style-large.css',
-            containers: '95%'
+            containers: '95%',
+            grid: { gutters: ['1.75em', '1.75em',] }
         },
         medium: {
             media: '(min-width: 769px) and (max-width: 1024px)',
             href: 'css/responsive/style-medium.css',
-            containers: '90%'
+            containers: '90%',
+            grid: { gutters: ['1.55em', '1.55em',] }
         },
         small: {
             media: '(max-width: 768px)',
             href: 'css/responsive/style-small.css',
-            containers: '100%'
+            containers: '100%',
+            grid: { gutters: ['1.25em', '1.25em',] }
         },
         xsmall: {
           media: '(max-width: 480px)',
           href: 'css/responsive/style-xsmall.css',
-          containers: '100%'
+          containers: '100%',
+          grid: { gutters: ['1em', '1em',] }
         }
     }
 });
